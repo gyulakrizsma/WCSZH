67421dde432cf5772279f10e2e3bce04ad6a52cd
gyula.krizsma <gyula.krizsma@hotmail.com>
2015-06-30 15:44:19 +0200

    Styling sections

--- public/javascripts/home/home.js
+++ public/javascripts/home/home.js
@@ -1,16 +1,38 @@
 (function(){
+    'use strict';
 
-    //MAP
+    var opened;
+
     function initialize() {
-        var mapCanvas = document.getElementById('wcszhmap');
-        var mapOptions = {
-            center: new google.maps.LatLng(47.381929, 8.536647),
-            zoom: 14,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-        var map = new google.maps.Map(mapCanvas, mapOptions)
+        var myLatlng = new window.google.maps.LatLng(47.381929, 8.536647);
+        var mapProp = {
+            center: myLatlng,
+            zoom:15,
+            scrollwheel: false,
+            mapTypeId:window.google.maps.MapTypeId.ROADMAP
+        };
+
+        var map = new window.google.maps.Map(document.getElementById('wcszhmap'), mapProp);
+
+        var marker = new window.google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            title: 'Atelier Tanz',
+            animation: window.google.maps.Animation.DROP,
+            //icon: '/images/home/carmarker.png'
+        });
     }
-    google.maps.event.addDomListener(window, 'load', initialize);
+    window.google.maps.event.addDomListener(window, 'load', initialize);
     //MAP
+
+    $('.inner-circle').mouseover(showOrHideInfo);
+
+    $('.inner-circle').mouseout(showOrHideInfo);
+
+    function showOrHideInfo() {
+
+        var info = $(this).data('info');
+        $('.' + info).toggle('slow');
+    }
 
 })();
